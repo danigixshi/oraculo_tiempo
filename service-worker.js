28a6441c26c99512ce774d3f08c0f816763edf0e
danigixshi <dangartoplogic@gmail.com>
2025-06-10 19:47:53 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+const CACHE_NAME = "oraculo-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
